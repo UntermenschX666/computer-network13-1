9526b083889bca0e2837bdf9f57651cf2e9d08c6
Untermensch <nikolay.kolesnikov.97@gmail.com>
2019-02-26 15:28:27 +0300

    Lab was done

--- checkTest.js
+++ checkTest.js
@@ -23,8 +23,32 @@ function checkTest() {
     if(answer5[0].checked)
         sum++;
 
-    result.textContent = (sum/5)*100  + "%";
+    result.textContent = (sum/5)*100  + "%" + " ответов верные";
     stop.textContent = "1";
     check.disabled = true;
+}
+
+function remake(){
+        var answer1 = document.getElementsByName("answer1");
+        var answer2 = document.getElementsByName("answer2");
+        var answer3 = document.getElementsByName("answer3");
+        var answer4 = document.getElementsByName("answer4");
+        var answer5 = document.getElementsByName("answer5");
+        answer1[0].checked = false;
+        answer1[1].checked = false;
+        answer1[2].checked = false;
+        answer2[0].checked = false;
+        answer2[1].checked = false;
+        answer2[2].checked = false;
+        answer3[0].checked = false;
+        answer3[1].checked = false;
+        answer3[2].checked = false;
+        answer4[0].checked = false;
+        answer4[1].checked = false;
+        answer4[2].checked = false;
+        answer5[0].checked = false;
+        answer5[1].checked = false;
+        refreshTimer();
+
 }
 
--- timer.js
+++ timer.js
@@ -1,26 +1,34 @@
-var timeObj = document.getElementById("time");
-var seconds = 0;
-var minutes = 2;
+var interval;
 
-function myTimer(){
-    var stop = document.getElementById("stop");
-    seconds--;
+function startTimer(minutes, seconds) {
+    var timeObj = document.getElementById("time");
+    function myTimer(){
+        var stop = document.getElementById("stop");
+        seconds--;
 
-    if(seconds === -1){
-        seconds = 59;
-        minutes -= 1;
+        if(seconds === -1){
+            seconds = 59;
+            minutes -= 1;
+        }
+
+        var timeString = minutes + ":" + seconds;
+        timeObj.textContent = timeString;
+        if(seconds === 0 && minutes === 0){
+            alert("Время вышло!");
+            document.location.href = "index.html";
+            clearInterval(interval);
+        }
+
+        if(stop.textContent == 1){
+            clearInterval(interval);
+
+        }
+
     }
-
-    var timeString = minutes + ":" + seconds
-    timeObj.textContent = timeString;
-    if(seconds === 0 && minutes === 0){
-        clearInterval(interval);
-    }
-
-    if(stop.textContent == 1)
-        clearInterval(interval);
-
+    interval = setInterval(myTimer, 1000);
 }
 
-var interval = setInterval(myTimer, 100);
-
+function refreshTimer() {
+    clearInterval(interval);
+    startTimer(2, 0)
+}
